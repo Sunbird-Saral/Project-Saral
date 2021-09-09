78c678c27ae1d8b514a9071bfc3ac86dae32e219
Apurvabisht97 <apurva.bisht@tarento.com>
2021-09-09 18:59:13 +0530

    update Class API

--- v1.0/backend/src/routers/class.route.js
+++ v1.0/backend/src/routers/class.route.js
@@ -4,6 +4,8 @@ const { auth } = require('../middleware/auth')
 const _ = require('lodash')
 const Promise = require('bluebird')
 const Student = require('../models/students')
+const Exam = require('../models/exams')
+const Mark = require('../models/marks')
 const router = new express.Router()
 
 router.post('/addClasses', auth, async (req, res) => {
@@ -92,47 +94,46 @@ router.post('/updateClass', auth, async (req, res) => {
     }
 })
 
-// router.delete('/deleteClass', auth, async (req, res) => {
-//     if (Object.keys(req.body).length === 0) res.status(400).send({ message: 'Validation error.' })
-//     const inputKeys = Object.keys(req.body)
-//     const allowedUpdates = ['classId']
-//     const isValidOperation = inputKeys.every((update) => allowedUpdates.includes(update))
+router.delete('/deleteClass', auth, async (req, res) => {
+    if (Object.keys(req.body).length === 0) res.status(400).send({ message: 'Validation error.' })
+    const inputKeys = Object.keys(req.body)
+    const allowedUpdates = ['classId']
+    const isValidOperation = inputKeys.every((update) => allowedUpdates.includes(update))
 
-//     if (!isValidOperation) {
-//         return res.status(400).send({ error: 'Invalid delete opration' })
-//     }
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid delete opration' })
+    }
 
-//     const match = {
-//         schoolId: req.school.schoolId,
-//         classId: req.body.classId
-//     }
+    const match = {
+        schoolId: req.school.schoolId,
+        classId: req.body.classId
+    }
 
-//     try {
-//         const classData = await ClassModel.findOne(match)
-//         if (classData) {
-//             await ClassModel.deleteOne(match)
-//             let lookup = {
-//                 schoolId: req.school.schoolId,
-//                 studentClass: { $elemMatch: { classId: req.body.classId } }
-//             }
-//             console.log(lookup)
-//             // let student = await Student.findOneAndRemove({
-//             //     schoolId: req.school.schoolId,
-//             //     studentClass: { $elemMatch: { classId: req.body.classId } }
-//             // })
-//             // await Student.deleteMany(lookup)
-//             // let exam = await Student.deleteMany(match)
-//             // console.log(exam)
-//             // await Mark.deleteMany({ schoolId: req.school.schoolId,studentId: student.studentId })
-//             res.status(200).send(classData)
-//         } else {
-//             res.status(404).send('Class does not exist.')
-//         }
-//     }
-//     catch (e) {
-//         console.log(e);
-//         res.status(400).send(e)
-//     }
-            // })
+    try {
+        const classData = await ClassModel.findOne(match)
+        if (classData) {
+            await ClassModel.deleteOne(match)
+            let lookup = {
+                schoolId: req.school.schoolId,
+                studentClass: { $elemMatch: { classId: req.body.classId } }
+            }
+            let students = await Student.find(lookup).lean()
+            if (students.length) {
+                for (let student of students) {
+                    await Mark.deleteMany({ schoolId: req.school.schoolId, studentId: student.studentId })
+                }
+                await Student.deleteMany(lookup).lean()
+            }
+            await Exam.deleteMany(match)
+            res.status(200).send({ "message": "Class has been deleted successfully." })
+        } else {
+            res.status(404).send({ "message": 'Class does not exist.' })
+        }
+    }
+    catch (e) {
+        console.log(e);
+        res.status(400).send(e)
+    }
+})
 
 module.exports = router
