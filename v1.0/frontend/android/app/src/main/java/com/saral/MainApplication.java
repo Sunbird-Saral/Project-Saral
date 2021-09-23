bafcda371faa991c3e685359e538f0fd5d6c1758
arman ansari <arman.ansari@tarento.com>
2021-09-23 12:28:30 +0530

    added npm

--- v1.0/frontend/android/app/src/main/java/com/saral/MainApplication.java
+++ v1.0/frontend/android/app/src/main/java/com/saral/MainApplication.java
@@ -4,6 +4,7 @@ import android.app.Application;
 import android.content.Context;
 import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
+import com.reactnativecommunity.asyncstorage.AsyncStoragePackage;
 import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -11,6 +12,8 @@ import com.facebook.react.modules.storage.ReactDatabaseSupplier;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
+import org.ekstep.saral.saralsdk.SaralSDKPackage;
+
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -30,7 +33,8 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new RNOpenCvCameraModelPackage());
+          // packages.add(new RNOpenCvCameraModelPackage());
+          packages.add(new SaralSDKPackage());
           return packages;
         }
 
