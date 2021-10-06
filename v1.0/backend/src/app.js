1a40be7d84ae4c60744f83f74a561da57a679dcf
Apurvabisht97 <apurva.bisht@tarento.com>
2021-10-06 11:58:18 +0530

    update read me

--- v1.0/backend/src/app.js
+++ v1.0/backend/src/app.js
@@ -15,7 +15,7 @@ const loggerMiddleware = (req, res, next) => {
     next()
 }
 app.use(express.json({limit: '50mb'}));
-app.use(bodyParser.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
+app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(express.json())
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
