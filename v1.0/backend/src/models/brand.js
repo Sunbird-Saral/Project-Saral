338eb126343034888151455cd27745b80645d30e
Apurvabisht97 <apurva.bisht@tarento.com>
2021-10-14 13:10:28 +0530

    update brand APIs

---
+++ v1.0/backend/src/models/brand.js
@@ -0,0 +1,33 @@
+const mongoose = require('mongoose')
+
+const BrandSchema = new mongoose.Schema({
+    logoImage:{
+        type: String,
+        required: true
+    },
+    themeColor1:{
+        type: String,
+        trim: true
+    },
+    themeColor2:{
+        type: String,
+        trim: true
+    },
+    Appname:{
+        type: String,
+        required: true,
+        trim: true
+    },
+    state:{
+        type: String,
+        required: true,
+        trim: true
+    }
+},{
+    timestamps: true
+})
+
+
+const Brand = mongoose.model('Brand', BrandSchema)
+
+module.exports = Brand
