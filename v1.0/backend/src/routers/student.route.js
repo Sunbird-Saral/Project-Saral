be8292588c3c517bece6b5592c4c5cf2ab7d8207
Apurvabisht97 <apurva.bisht@tarento.com>
2021-10-18 09:48:05 +0530

    UPDATE student availability field

--- v1.0/backend/src/routers/student.route.js
+++ v1.0/backend/src/routers/student.route.js
@@ -95,14 +95,16 @@ router.post('/fetchStudentsandExamsByQuery', auth, async (req, res) => {
         let studentIds = students.map(id=>id.studentId)
         const marks = await Marks.StudentsMark(studentIds)
         
+        if(marks.length){
         for(let student of students){
         for(let mark of marks){
-            if(student.studentId == mark.studentId){
-                student["studentAvailability"] = mark.studentAvailability
+                    if(student.studentId == mark.studentId)    student["studentAvailability"] = mark.studentAvailability
+                }
+                }
             }else{
+            for(let student of students){
                 student["studentAvailability"] = "true"
             }
-        }
         }
         const exams = await Exam.find(examMatch, { _id: 0, __v: 0, createdAt: 0, updatedAt: 0 }).lean()
         res.send({ students, exams })
