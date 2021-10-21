39fdee9245cbe4ecc4e0aa75c0ebb0920fd9111b
Dileep Gadiraju <89640648+dileep-gadiraju@users.noreply.github.com>
2021-10-21 17:02:32 +0530

    fixing btoa error refer issue#71

--- v1.0/frontend/SaralApp/index.js
+++ v1.0/frontend/SaralApp/index.js
@@ -5,5 +5,15 @@
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
+import {decode, encode} from 'base-64'
 
 AppRegistry.registerComponent(appName, () => App);
+
+
+if (!global.btoa) {
+    global.btoa = encode;
+}
+
+if (!global.atob) {
+    global.atob = decode;
+}
