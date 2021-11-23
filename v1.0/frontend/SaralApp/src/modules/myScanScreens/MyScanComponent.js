b619e66ac176f9c39fa61bedf9ff252149c3702d
Apurvabisht97 <apurva.bisht@tarento.com>
2021-11-23 15:41:44 +0530

    update myscan component file

--- v1.0/frontend/SaralApp/src/modules/myScanScreens/MyScanComponent.js
+++ v1.0/frontend/SaralApp/src/modules/myScanScreens/MyScanComponent.js
@@ -213,10 +213,12 @@ class MyScanComponent extends Component {
             for (let j = 0; j < cells[i].rois.length; j++) {
 
                 marks = marks + cells[i].rois[j].result.prediction,
-                roisData.layout.cells[i].predictionConfidence = cells[i].rois[j].result.confidence
+                predictionConfidenceArray.push(cells[i].rois[j].result.confidence)
+                // roisData.layout.cells[i].predictionConfidence = cells[i].rois[j].result.confidence
 
             }
             roisData.layout.cells[i].consolidatedPrediction = marks
+            roisData.layout.cells[i].predictionConfidence = predictionConfidenceArray
             if (roisData.layout.cells[i].format.value === neglectData[0] || roisData.layout.cells[i].format.name.length-3 == neglectData[0].length) {
                 roisData.layout.cells[i].studentIdPrediction = marks
             } else {
