d1c73a852ed41f930067ae4dcb4cdd67c274b302
Apurvabisht97 <apurva.bisht@tarento.com>
2021-11-25 17:06:31 +0530

    update studentIdTraining data for multiple student

--- v1.0/frontend/SaralApp/src/modules/ScannedDetails/ScannedDetailsComponent.js
+++ v1.0/frontend/SaralApp/src/modules/ScannedDetails/ScannedDetailsComponent.js
@@ -343,7 +343,7 @@ const ScannedDetailsComponent = ({
                 }
 
                 stdData.studentId = el.RollNo
-                let putTrainingData = loginData.data.school.storeTrainingData ? stdData.studentIdTrainingData = storeTrainingData.length > 0 ? storeTrainingData[0].trainingDataSet : '' : ''
+                let putTrainingData = loginData.data.school.storeTrainingData ? stdData.studentIdTrainingData = storeTrainingData.length > 0 ? storeTrainingData[index].trainingDataSet : '' : ''
 
 
                 let stdMarks_info = []
