d63d519645e2a3f31aa3533dda3dee6c377b759f
arman ansari <arman.ansari@tarento.com>
2021-12-02 20:00:57 +0530

    added demo of background data saving in db

--- v1.0/frontend/SaralApp/src/navigator/AppNavigator.js
+++ v1.0/frontend/SaralApp/src/navigator/AppNavigator.js
@@ -16,19 +16,25 @@ import ScanStatus from "../modules/ScanStatus/ScanStatus";
 import DashboardComponent from '../modules/myScanScreens/DashboardComponent'
 import ScannedDetailsComponent from "../modules/ScannedDetails/ScannedDetailsComponent";
 import HomeComponent from "../modules/myScanScreens/Homescreen"
-
+import { bindActionCreators } from "redux";
+import { connect, useDispatch } from "react-redux";
+import bgFlag from './../flux/reducers/bgFlag'
+import { storeFactory } from '../flux/store/store'
+import C from '../flux/actions/constants'
+import NetInfo from "@react-native-community/netinfo";
+import PushNotification, { Importance } from "react-native-push-notification";
 
 const AuthStack = createStackNavigator({
     login: {
         screen: LoginComponent
-    }   
+    }
 }, {
     initialRouteName: 'login',
     headerMode: 'none'
 })
 
 const MainStack = createStackNavigator(
-    { 
+    {
         dashboard: {
             screen: DashboardComponent
         },
@@ -69,6 +75,66 @@ const MainStack = createStackNavigator(
     }
 )
 
+function flagAction(payload) {
+    return {
+        type: C.BACKGROUND_FLAG,
+        payload
+    }
+}
+
+const checkNetworkConnectivity = () => {
+    const subscribe = NetInfo.addEventListener(state => {
+        return state.isConnected
+    })
+    return subscribe()
+}
+
+
+const testPushNotification = (title, msg) => {
+
+    PushNotification.createChannel(
+        {
+            channelId: "1dzD0LtdsIe6n1OalyqYdQZvrFv1", // (required)
+            channelName: "Notifications", // (required)
+            channelDescription: "A channel to categorise your notifications", // (optional) default: undefined.
+            soundName: "notification.mp3",
+            importance: 4,
+            vibrate: true,
+        });
+
+    PushNotification.configure({
+        permissions: {
+            alert: true,
+            badge: true,
+            sound: true,
+        },
+        popInitialNotification: true,
+        requestPermissions: true
+    })
+
+    PushNotification.localNotification({
+        channelId: "1dzD0LtdsIe6n1OalyqYdQZvrFv1",
+        smallIcon: "ic_notification",
+        color: "white",
+        vibrate: true,
+        shortcutId: "shortcut-id",
+        title: title,
+        message: msg,
+        playSound: true,
+        soundName: "notification.mp3",
+        priority: "high"
+    });
+}
+
+
+setTimeout(() => {
+    storeFactory.dispatch(flagAction(true))
+    if (checkNetworkConnectivity()) {
+        testPushNotification("Uploading...☻☻☻", "please wait we are uploading")
+    }
+}, 9000);
+
+
 const AppNavigation = createSwitchNavigator(
     {
         auth: AuthStack,
@@ -79,4 +145,11 @@ const AppNavigation = createSwitchNavigator(
     }
 );
 
-export default (AppContainer = createAppContainer(AppNavigation));
+
+const mapDispatchToProps = (dispatch) => {
+    return bindActionCreators({
+        bgFlag: bgFlag
+    }, dispatch)
+}
+
+export default connect(null, mapDispatchToProps)(AppContainer = createAppContainer(AppNavigation));
