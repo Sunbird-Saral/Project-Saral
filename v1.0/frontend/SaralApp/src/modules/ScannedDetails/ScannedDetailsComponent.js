2916eec321b32b302baf25809cf9aaf45ba427b0
navin9584 <navin.nahar@tarento.com>
2022-02-22 12:32:23 +0530

    add checkbox auto select if student id '00000'

--- v1.0/frontend/SaralApp/src/modules/ScannedDetails/ScannedDetailsComponent.js
+++ v1.0/frontend/SaralApp/src/modules/ScannedDetails/ScannedDetailsComponent.js
@@ -112,6 +112,11 @@ const ScannedDetailsComponent = ({
 
         let absent = datas.filter((item) => item.studentId == studentId & item.studentAvailability == false)
 
+        if(studentId == 0){
+            setToggleCheckBox(true)
+        }else{
+            setToggleCheckBox(false)
+        }
         if (absent.length > 0) {
             setStdErr("Student is Absent")
             setStudentValid(false)
