e6aa547dce326f773fec09c85491c20959804d5b
Apurvabisht97 <apurva.bisht@tarento.com>
2022-04-06 09:55:24 +0530

    update backend changes for support email

--- v1.0/backend/src/routers/school.route.js
+++ v1.0/backend/src/routers/school.route.js
@@ -71,7 +71,8 @@ router.post('/schools/login', async (req, res) => {
             autoSync: schools.autoSync,
             autoSyncFrequency: schools.autoSyncFrequency,
             tags: schools.tags,
-            autoSyncBatchSize: schools.autoSyncBatchSize
+            autoSyncBatchSize: schools.autoSyncBatchSize,
+            supportEmail: schools.supportEmail
         }
 
         let response = {
