c015b3d0b3297c2514ac0d39e90a635dd5fdcd72
Apurvabisht97 <apurva.bisht@tarento.com>
2022-06-17 19:35:36 +0530

    update changes related to book distribution roi generation

--- v1.0/backend/src/app.js
+++ v1.0/backend/src/app.js
@@ -11,6 +11,7 @@ const examRouter = require('./routers/exam.route')
 const markRouter = require('./routers/mark.route')
 const roiRouter = require('./routers/roi.route')
 const brandRouter = require('./routers/brand.route')
+var cors = require('cors');
 
 const spec = fs.readFileSync(`${__dirname}/swagger-saral-frontend.yaml`, 'utf-8');
 const spec2 = fs.readFileSync(`${__dirname}/swagger-saral-maintenance.yaml`, 'utf-8');
@@ -26,6 +27,7 @@ const loggerMiddleware = (req, res, next) => {
 app.use(express.json({limit: '50mb'}));
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(express.json())
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 // Register the function as middleware for the application
