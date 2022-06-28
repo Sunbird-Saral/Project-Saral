d3bad25327379f1847d0a6d40e81a39c57023402
Apurvabisht97 <apurva.bisht@tarento.com>
2022-06-28 11:22:11 +0530

    update version for new release

--- v1.0/frontend/SaralApp/src/configs/config.js
+++ v1.0/frontend/SaralApp/src/configs/config.js
@@ -4,6 +4,6 @@ const configs = {
 
 export default configs;
 
-export const apkVersion =  "1.5.3"
+export const apkVersion =  "1.5.5"
 export const apkVersionId = "1"
 export const apkURL = 'http://play.google.com/store/apps/details?id=com.up_saraldata'
