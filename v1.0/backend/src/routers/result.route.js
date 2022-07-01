138f5874bcfe37cd4aa6fa3603585e45c078f1ee
Apurvabisht97 <apurva.bisht@tarento.com>
2022-07-01 12:14:36 +0530

    update result API

--- v1.0/backend/src/routers/result.route.js
+++ v1.0/backend/src/routers/result.route.js
@@ -10,7 +10,7 @@ router.put('/saveResults', auth, async (req, res) => {
     const category3 = req.body.category3
     const date = req.body.date
     const orgId = req.school.orgId
-    const category1  = req.body.category1 
+    const category1 = req.body.category1
     const createdOn = new Date().getTime()
     const roiId = req.body.roiId
 
@@ -20,7 +20,7 @@ router.put('/saveResults', auth, async (req, res) => {
             orgId,
             date,
             category3,
-            category1 ,
+            category1,
             createdOn,
             roiId
         })
@@ -28,16 +28,19 @@ router.put('/saveResults', auth, async (req, res) => {
     });
     try {
         for (let data of results) {
-         
-            let studentResultsExist = await Result.findOne({ orgId:data.orgId,identifier: data.identifier,category1 :data.category1 ,category3: data.category3, date: data.date, roiId: data.roiId  })
+            if (!data.date && data.date == undefined) {
+                data.date = new Date().toLocaleDateString()
+            }
+
+            let studentResultsExist = await Result.findOne({ orgId: data.orgId, identifier: data.identifier, category1: data.category1, category3: data.category3, date: data.date, roiId: data.roiId })
             if (!studentResultsExist) {
                 await Result.create(data)
             } else {
-                if (data.orgId == studentResultsExist.orgId && data.identifier == studentResultsExist.identifier && data.category1  == studentResultsExist.category1  && data.category3 == studentResultsExist.category3 && data.date  == studentResultsExist.date) {
+                if (data.orgId == studentResultsExist.orgId && data.identifier == studentResultsExist.identifier && data.category1 == studentResultsExist.category1 && data.category3 == studentResultsExist.category3 && data.date == studentResultsExist.date) {
                     let lookup = {
                         identifier: data.identifier
                     }
-                    let update = { $set: { identifierTrainingData: data.identifierTrainingData,predictedIdentifier: data.predictedIdentifier,availability: data.availability, resultInfo: data.resultInfo ,maxResultTrainingData: data.maxResultTrainingData,maxResultPredicted: data.maxResultPredicted, securedResult: data.securedResult, totalResult: data.totalResult,obtainedResultTrainingData: data.obtainedResultTrainingData,obtainedResultPredicted: data.obtainedResultPredicted  } }
+                    let update = { $set: { identifierTrainingData: data.identifierTrainingData, predictedIdentifier: data.predictedIdentifier, availability: data.availability, resultInfo: data.resultInfo, maxResultTrainingData: data.maxResultTrainingData, maxResultPredicted: data.maxResultPredicted, securedResult: data.securedResult, totalResult: data.totalResult, obtainedResultTrainingData: data.obtainedResultTrainingData, obtainedResultPredicted: data.obtainedResultPredicted } }
                     await Result.update(lookup, update)
                 }
             }
