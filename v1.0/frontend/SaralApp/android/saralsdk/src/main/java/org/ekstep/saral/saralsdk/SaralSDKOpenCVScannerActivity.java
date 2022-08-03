d88c2f144e488c6e49e56d4688dec09e6f482801
navin9584 <navin.nahar@tarento.com>
2022-08-03 16:12:25 +0530

    change color

--- v1.0/frontend/SaralApp/android/saralsdk/src/main/java/org/ekstep/saral/saralsdk/SaralSDKOpenCVScannerActivity.java
+++ v1.0/frontend/SaralApp/android/saralsdk/src/main/java/org/ekstep/saral/saralsdk/SaralSDKOpenCVScannerActivity.java
@@ -493,12 +493,11 @@ public class SaralSDKOpenCVScannerActivity extends ReactActivity implements Came
     private void showProcessingInformation(Mat image) {
         String text     = "Layout image captured, processing for results !!";
         Point position  = new Point(image.width()/6, image.height() / 2);
-        Scalar color    = new Scalar(46,139,87);
+        Scalar color    = new Scalar(0,100,0);
         int font        = Imgproc.COLOR_BGR5652GRAY;
         int scale       = 2;
         int thickness   = 3;
         Imgproc.putText(image, text, position, font, scale, color, thickness);
-
     }
 
     // }
