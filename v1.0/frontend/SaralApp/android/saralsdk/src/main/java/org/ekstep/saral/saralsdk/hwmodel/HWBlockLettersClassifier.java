79465a924447b78da90b60ae91017f590b8355e2
Apurvabisht97 <apurva.bisht@tarento.com>
2022-08-19 14:26:00 +0530

    revert model name changes for alphanumeric

--- v1.0/frontend/SaralApp/android/saralsdk/src/main/java/org/ekstep/saral/saralsdk/hwmodel/HWBlockLettersClassifier.java
+++ v1.0/frontend/SaralApp/android/saralsdk/src/main/java/org/ekstep/saral/saralsdk/hwmodel/HWBlockLettersClassifier.java
@@ -31,7 +31,7 @@ public class HWBlockLettersClassifier {
      * Name of the model file hosted with Firebase.
      */
     private static final String HOSTED_MODEL_NAME = null;
-    private static final String LOCAL_MODEL_ASSET = "model_letter_digits_finetune_17_08_22_epoch_15.tflite";
+    private static final String LOCAL_MODEL_ASSET = "model_letter_digits_finetune_18_07_22_epoch_15.tflite";
 
     /**
      * Dimensions of inputs.
