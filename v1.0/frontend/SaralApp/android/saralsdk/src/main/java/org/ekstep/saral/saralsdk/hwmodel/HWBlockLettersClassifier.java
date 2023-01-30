da397d045a0585643d180c1c917e2cbdb2bf76fe
Apurvabisht97 <apurva.bisht@tarento.com>
2023-01-30 13:19:22 +0530

    update model name

--- v1.0/frontend/SaralApp/android/saralsdk/src/main/java/org/ekstep/saral/saralsdk/hwmodel/HWBlockLettersClassifier.java
+++ v1.0/frontend/SaralApp/android/saralsdk/src/main/java/org/ekstep/saral/saralsdk/hwmodel/HWBlockLettersClassifier.java
@@ -32,7 +32,7 @@ public class HWBlockLettersClassifier {
      */
     private static final String HOSTED_MODEL_NAME = null;
     // private static final String LOCAL_MODEL_ASSET = "model_letter_digits_finetune_18_07_22_epoch_15.tflite";
-    private static final String LOCAL_MODEL_ASSET = "saral_hwdaln_model.tflite";
+    private static final String LOCAL_MODEL_ASSET = "new_saral_hwdaln_model.tflite";
     /**
      * Dimensions of inputs.
      */
