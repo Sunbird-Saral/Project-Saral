01f223acea437925d82d621fca615f001def069b
Apurvabisht97 <apurva.bisht@tarento.com>
2023-03-21 16:00:00 +0530

    update fetchstudentsandexambyquery API

--- v1.0/backend/src/controller/studentController.js
+++ v1.0/backend/src/controller/studentController.js
@@ -10,10 +10,10 @@ exports.fetchStudentsandExams = async (req, res, next) => {
     const examMatch = {}
 
     match.schoolId = req.school.schoolId
-    if (req.body.classId) {
+    if (req.query.classId) {
         let studentClassObj = {
-            classId: req.body.classId,
-            className: `Class-${req.body.classId}`
+            classId: req.query.classId,
+            className: `Class-${req.query.classId}`
         }
         let studentClass = [studentClassObj]
         match.studentClass = studentClass
@@ -23,12 +23,12 @@ exports.fetchStudentsandExams = async (req, res, next) => {
         return res.status(404).json({ message: 'Please send classId' })
     }
 
-    if (req.body.section && req.body.section != "0") {
-        match.section = req.body.section
+    if (req.query.section && req.query.section != "0") {
+        match.section = req.query.section
     }
 
-    if (req.body.hasOwnProperty('subject')) {
-        let subject = req.body.subject.split(' ')
+    if (req.query.hasOwnProperty('subject')) {
+        let subject = req.query.subject.split(' ')
         examMatch.subject = subject[0]
         examMatch.examDate = subject[1]
     }
@@ -47,8 +47,8 @@ exports.fetchStudentsandExams = async (req, res, next) => {
                 examDate: examMatch.examDate
             }
 
-            if(req.body.set){
-                lookup.set = req.body.set 
+            if(req.query.set){
+                lookup.set = req.query.set 
             }
         
             let marks = await Marks.findOne(lookup) 
