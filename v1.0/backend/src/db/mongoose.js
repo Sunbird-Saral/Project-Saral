9fda6d994e8a5eec631ec111cf3c2456e05254b0
Apurvabisht97 <apurva.bisht@tarento.com>
2023-04-05 16:48:20 +0530

    update poolsize

--- v1.0/backend/src/db/mongoose.js
+++ v1.0/backend/src/db/mongoose.js
@@ -2,10 +2,12 @@ const mongoose = require('mongoose')
 
 // url/databasename
 const connectionURL = process.env.MONGODB_URL
+const poolSize = process.env.POOL_SIZE
 
 mongoose.connect(connectionURL, {
     useNewUrlParser: true,
     useCreateIndex: true,
     useUnifiedTopology: true,
+    poolSize: poolSize,
     useFindAndModify: false, // to handle deprication warning for findByIdandUpdate
 })
