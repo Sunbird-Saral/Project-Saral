b1cb58bd3ba28f02bde3e8d6435cbf0e46e0b8ef
Apurvabisht97 <apurva.bisht@tarento.com>
2023-04-10 12:18:34 +0530

    remove xl sheet from doc folder

--- v1.0/backend/src/app.js
+++ v1.0/backend/src/app.js
@@ -2,7 +2,6 @@ const express = require('express')
 require('./db/mongoose')
 var path = require('path');
 const fs = require('fs')
-const utils = require('util')
 const puppeteer = require('puppeteer')
 const yaml = require('js-yaml');
 const swaggerUi = require('swagger-ui-express');
@@ -49,7 +48,7 @@ const generateJestReportPdf = async () => {
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(express.json())
-// app.use(cors());
+app.use(cors());
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 // Register the function as middleware for the application
