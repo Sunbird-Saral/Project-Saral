fb947a4279198e6d9762d3e94a804b6fc7b30295
SanthoshKumarCS3624 <santhosh.murthy@tarento.com>
2023-04-10 14:40:33 +0530

    new loadtest py file and Latest readme file

--- v1.0/backend/data/Loadtest/test_data_generation_v4.py
+++ v1.0/backend/data/Loadtest/test_data_generation_v4.py
@@ -1,5 +1,6 @@
 import json
 import random
+import csv
 
 # To create School, Users and Student json files & Data to be written
 schoolsData=[]
@@ -37,6 +38,7 @@ for schools in range(0,schoolsCount):
 
 
 #Users objects to be appended
+
 countSchoolID = 0
 
 for users in range(0,usersCount):
@@ -115,4 +117,54 @@ with open("classes.json", "w") as fClass:
     json.dump(classData, fClass, indent=4)
 
 
-print("JSON files are created successfully")
+print("schools,users,students,classes JSON files are created successfully")
+
+
+
+###############################################
+
+
+
+def jsontocsv():
+
+  with open("./users.json") as file:
+      userdata = json.load(file)
+
+  fname = "UsernamePassword.csv"
+
+  with open(fname, "w") as file:
+      csv_file = csv.writer(file,lineterminator='\n')
+      csv_file.writerow(["userId","Password","schoolId"])
+      for item in userdata:
+          csv_file.writerow([item['userId'],"tarento@123",item['schoolId']])
+
+
+
+  # read JSON data from a file
+  with open('students.json') as f:
+      data = json.load(f)
+
+  # create CSV files
+  with open('classId_4to8.csv', mode='w', newline='') as f1, \
+      open('classId_1to3.csv', mode='w', newline='') as f2:
+
+      # create CSV writers
+      writer1 = csv.writer(f1)
+      writer2 = csv.writer(f2)
+
+      # write header row to both files
+      writer1.writerow(['StudentID4to8', 'name', 'section', 'schoolId', 'classId', 'className'])
+      writer2.writerow(['StudentID1to3', 'name', 'section', 'schoolId', 'classId', 'className'])
+
+      # loop over students in the JSON data
+      for student in data:
+          classId = int(student['classId'])
+          if classId > 3:
+              writer1.writerow([student['studentId'], student['name'], student['section'], student['schoolId'], classId, student['className']])
+          else:
+              writer2.writerow([student['studentId'], student['name'], student['section'], student['schoolId'], classId, student['className']])
+
+
+jsontocsv()
+
+print('Converted files to CSV and Test data generation is completed sucessfully')
