26a1fd9ebc2d792b4dc53ed84359874a6c1ebe9a
Apurvabisht97 <apurva.bisht@tarento.com>
2023-04-11 14:57:48 +0530

    change db name

--- v1.0/backend/src/models/brand.js
+++ v1.0/backend/src/models/brand.js
@@ -22,10 +22,6 @@ const BrandSchema = new mongoose.Schema({
         type: String,
         trim: true
     },
-    SchoolId:{
-        type: String,
-        trim: true
-    },
     screenLabels: {
         selectDetails: [{
             _id: false,
