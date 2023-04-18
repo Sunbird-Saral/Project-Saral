e1acf5cb583d56968a8196dd13584021e2750d58
vky25 <93203895+vky25@users.noreply.github.com>
2023-04-18 10:43:44 +0530

    Create providers.tf

---
+++ DevOps/Terraform-EKS/providers.tf
@@ -0,0 +1,34 @@
+# Provider Configuration
+#
+
+provider "aws" {
+  region  = "us-west-2"
+  version = ">= 2.38.0"
+}
+
+# Using these data sources allows the configuration to be
+# generic for any region.
+data "aws_region" "current" {}
+
+data "aws_availability_zones" "available" {}
+
+# Not required: currently used in conjuction with using
+# icanhazip.com to determine local workstation external IP
+# to open EC2 Security Group access to the Kubernetes cluster.
+# See workstation-external-ip.tf for additional information.
+provider "http" {}
+
+
+########################################
+##terraform {
+##  required_providers {
+##    aws = {
+##      source  = "hashicorp/aws"
+##      version = ">= 2.38.0"
+##    }
+##  }
+##}
+
+##provider "aws" {
+##  region = "us-west-2"
+##}
