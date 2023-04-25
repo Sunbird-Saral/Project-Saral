cb1bc5a628c122c7abcf9a1545f6053d668013a7
Apurvabisht97 <apurva.bisht@tarento.com>
2023-04-25 09:59:37 +0530

    remove comment from hash function

--- v1.0/backend/src/models/users.js
+++ v1.0/backend/src/models/users.js
@@ -51,7 +51,7 @@ userSchema.pre('save', async function (next) {
     const user = this
 
     if (user.isModified('password')) {
-        user.password = await bcrypt.hash(user.password, 8, { $comment: "Password Encryption."})
+        user.password = await bcrypt.hash(user.password, 8)
     }
 
     next()
