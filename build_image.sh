834e5b19c700a76ae743493505cd903e5b0bafb4
vky25 <93203895+vky25@users.noreply.github.com>
2023-05-14 10:58:14 +0530

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -1,1 +1,21 @@
+#! /bin/bash
 
+cd v1.0/backend
+if [ -f "$(pwd)/build" ]
+then
+sudo rm -rf $(pwd)/build
+fi
+if [  $( docker ps -q -f status=exited --filter "name=$JOB_BASE_NAME" ) ]
+then
+docker rm "$JOB_BASE_NAME"
+fi
+commit_id=${BUILD_ID}-$(git rev-parse --short HEAD)
+echo $commit_id> commit_id.txt
+cp ../../specs/v1.5/swagger-saral-maintenance.yaml ./src
+cp ../../specs/v1.5/swagger-saral-frontend.yaml ./src
+cp ../../specs/v1.5/swagger-saral-apidoc.yaml ./src
+                                          
+
+docker build -t saaral123/$image_name:$image_tag .
+docker login -u saaral123 -p Saral@123
+docker push saaral123/$image_name:$image_tag
