61def73a15fa2499c444dfc9358a1b00f156c8b0
navin9584 <navin.nahar@tarento.com>
2023-05-22 10:40:37 +0530

    add changes

--- v1.0/backend/src/logging/logger.js
+++ v1.0/backend/src/logging/logger.js
@@ -62,65 +62,31 @@ const winston = require('winston');
 // const { combine, timestamp, label, printf } = format;
 const jwt = require('jsonwebtoken')
 
-// const myFormat = format.printf(({level, meta, timestamp})=>{
-
-//   //  const token = JSON.stringify(meta) && JSON.stringify(meta.req) && JSON.stringify(meta.req.headers['authorization']).replace('Bearer ', '')
-  
-//   // // const decoded = jwt.verify(token, process.env.JWT_SECRET)
-//   // console.log('decoded',token);
-
-  // const deviceId =  JSON.stringify(meta) && JSON.stringify(meta.req.headers)  && JSON.stringify(meta.req.headers['x-request-deviceid']) != undefined && JSON.stringify(meta.req.headers['x-request-deviceid']) 
-  // console.log('deviceId?>>>>>>>>>>>',deviceId == undefined && '');
-  
-//   return `${timestamp} ${level} "deviceID : ${deviceId == undefined ? '' : deviceId}`
-// })
-
-const myFormat = winston.format.printf(({ level, message, meta, timestamp, data }) => {
-  console.log('meta>>>>',meta);
-    const deviceId =  JSON.stringify(meta) && JSON.stringify(meta.req.headers)  && JSON.stringify(meta.req.headers['x-request-deviceid']) 
-    const token = JSON.stringify(meta) && JSON.stringify(meta.req) && JSON.stringify(meta.req.headers['authorization'])
-    // const decoded = jwt.verify(token, process.env.JWT_SECRET)
-  
-    //  console.log('decoded?>>>>>>>>>>>',decoded);
-  
-  return `${timestamp}  ${level} "deviceID" : ${deviceId} "token" : ${token}  ${message} `;
-});
-
 const logConfiguration = {
   transports: [
       new winston.transports.Console()
   ],
   format: winston.format.combine(
+    winston.format.colorize(),
+    winston.format.json(),
       winston.format.label({
           label: `Label🏷️`
       }),
       winston.format.timestamp({
          format: 'MMM-DD-YYYY HH:mm:ss'
      }),
-      winston.format.printf(info => `${info.level}: ${info.label}: ${[info.timestamp]}
+     
+      winston.format.printf(info => 
+        `${info.level}: ${info.label}: ${[info.timestamp]}
        ${info.message}
-       "deviceID" :${JSON.stringify(info.meta) && JSON.stringify(info.meta.req.headers)  && JSON.stringify(info.meta.req.headers['x-request-deviceid'])}
-       "token" :${JSON.stringify(info.meta) && JSON.stringify(info.meta.req) && JSON.stringify(info.meta.req.headers['authorization'])}
+       "deviceID" : ${ JSON.stringify(info.meta) && JSON.stringify(info.meta.req.headers)  && JSON.stringify(info.meta.req.headers['x-request-deviceid'])!= undefined ? JSON.stringify(info.meta.req.headers['x-request-deviceid']): null}
+       "token" :${JSON.stringify(info.meta) && JSON.stringify(info.meta.req) && JSON.stringify(info.meta.req.headers['authorization']) != undefined ? JSON.stringify(info.meta.req.headers['authorization']): null}
        `),
        
-  )
+  ),
 };
 
 const logger = winston.createLogger(logConfiguration);
-// logger.info("Hello, Winston logger, some info!");
-
-// const logger = createLogger({
-//   transports: [
-//     new transports.Console()
-// ],
-// // exitOnError: false,
-// format : format.combine(
-//     format.json(),
-//     format.timestamp(),
-//     // format.prettyPrint(),
-//     myFormat
-// )
-// })
 
 module.exports = logger;
 
