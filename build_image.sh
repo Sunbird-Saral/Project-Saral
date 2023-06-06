e4345493d3c6f3c8b5913e5ae7ce7b93d462573f
vky25 <93203895+vky25@users.noreply.github.com>
2023-06-06 15:13:47 +0530

    Update build_image.sh

--- build_image.sh
+++ build_image.sh
@@ -1,22 +1,34 @@
-#! /bin/bash
+#!/bin/bash
 
 cd v1.0/backend
-if [ -f "$(pwd)/build" ]
-then
-sudo rm -rf $(pwd)/build
+
+if [ -f "$(pwd)/build" ]; then
+    sudo rm -rf $(pwd)/build
 fi
-if [  $( docker ps -q -f status=exited --filter "name=$JOB_BASE_NAME" ) ]
-then
-docker rm "$JOB_BASE_NAME"
+
+if [ $(docker ps -q -f status=exited --filter "name=$JOB_BASE_NAME") ]; then
+    docker rm "$JOB_BASE_NAME"
 fi
+
 commit_id=${BUILD_ID}-$(git rev-parse --short HEAD)
-echo $commit_id> commit_id.txt
+echo $commit_id > commit_id.txt
+
 cp ../../specs/v1.5/swagger-saral-maintenance.yaml ./src
 cp ../../specs/v1.5/swagger-saral-frontend.yaml ./src
 cp ../../specs/v1.5/swagger-saral-apidoc.yaml ./src
 
 image_name="backendapi"
 image_tag="2.0"
+
+# Generate a dynamic tag based on the build number
+image_tag="${image_tag}-$BUILD_NUMBER"
+
+# Build and tag the Docker image
+docker build -t "$image_name:$image_tag" .
+
+# Push the Docker image to a registry if needed
+docker push "$image_name:$image_tag"
+
                                           
 
 docker build -t saaral123/$image_name:$image_tag .
