058db8cb8f24b2eb1850ca408147335326ff2c91
vky25 <93203895+vky25@users.noreply.github.com>
2023-08-04 13:49:20 +0530

    Update mongoose.js

--- v1.0/backend/src/db/mongoose.js
+++ v1.0/backend/src/db/mongoose.js
@@ -30,8 +30,8 @@ const poolFactory = {
 };
 
 const pool = createPool(poolFactory, {
-  max: 300,
-  min: 100, // Minimum number of connections in the pool,
+  max: 100,
+  min: 50, // Minimum number of connections in the pool,
   autostart: true,
   idleTimeoutMillis: 30000, // How long a resource can stay idle in the pool before being removed (30 seconds in this case)
 });
