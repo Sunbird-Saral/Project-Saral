a52a158bb6214ee178c58bb2d3ef655299fe3c64
rsankagi <rashmi.ankalgi@tarento.com>
2023-08-30 11:55:14 +0530

    Changes done for UP

--- v1.0/frontend/SaralApp/android/app/src/main/java/com/saralapp/MainApplication.java
+++ v1.0/frontend/SaralApp/android/app/src/main/java/com/saralapp/MainApplication.java
@@ -1,4 +1,4 @@
-package com.saralapp;
+package com.up_saralapp;
 
 import android.app.Application;
 import android.content.Context;
@@ -67,7 +67,7 @@ public class MainApplication extends Application implements ReactApplication {
          We use reflection here to pick up the class that initializes Flipper,
         since Flipper library is not available in release mode
         */
-        Class<?> aClass = Class.forName("com.saralapp.ReactNativeFlipper");
+        Class<?> aClass = Class.forName("com.up_saralapp.ReactNativeFlipper");
         aClass
             .getMethod("initializeFlipper", Context.class, ReactInstanceManager.class)
             .invoke(null, context, reactInstanceManager);
