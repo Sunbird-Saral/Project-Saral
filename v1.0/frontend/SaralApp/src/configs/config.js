c9a6e7c5fd06a012512e74950a46ffa0abfd7afa
rsankagi <rashmi.ankalgi@tarento.com>
2023-09-07 20:04:45 +0530

    Firebase integration

--- v1.0/frontend/SaralApp/src/configs/config.js
+++ v1.0/frontend/SaralApp/src/configs/config.js
@@ -4,7 +4,7 @@ const configs = {
 
 export default configs;
 
-export const apkVersion = '1.6.0';
+export const apkVersion = '1.6.1';
 export const apkVersionId = '1';
 export const apkURL =
   'https://play.google.com/store/apps/details?id=com.up_saralapp';
