64c2e5705822c46b0d74af1bdb7257181cd8bed4
rsankagi <rashmi.ankalgi@tarento.com>
2023-09-19 11:36:31 +0530

    version update

--- v1.0/frontend/SaralApp/src/configs/config.js
+++ v1.0/frontend/SaralApp/src/configs/config.js
@@ -5,6 +5,6 @@ const configs = {
 export default configs;
 
 export const apkVersion = '1.6.1';
-export const apkVersionId = '1';
+export const apkVersionId = '1.3';
 export const apkURL =
   'https://play.google.com/store/apps/details?id=com.up_saralapp';
