a07d8b344b612795a1f088248d869197598e799e
venky3692 <venkatesh.iyer@tarento.com>
2023-10-07 11:16:40 +0530

    adding trail dataset and code to convert pdfs to images

---
+++ poc-hackathon/src/pdf_to_jpeg.py
@@ -0,0 +1,15 @@
+from pdf2image import convert_from_path
+import os
+class Convert_image:
+    def __init__(self, path_to_read, path_to_write):
+        self.path_to_read = path_to_read
+        self.path_to_write = path_to_write
+    
+    def convert_to_jpeg(self):
+        for pdf in os.listdir(self.path_to_read):
+            image = convert_from_path(self.path_to_read+pdf, 500)
+            for count, page in enumerate(image):
+                page.save(pdf +'_'+str(count)+'.jpeg', 'JPEG')
+
+c = Convert_image(path_to_read='./trial_dataset/pdfs/', path_to_write='./trail_dataset/jpegs/')
+c.convert_to_jpeg()
