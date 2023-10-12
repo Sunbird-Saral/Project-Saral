c579bb1c51d722f5694ee52a9347e2dd90cbdebd
sanjay-v46 <sanjay@tarento.com>
2023-10-12 23:24:19 +0530

    fix sonar reported security issues and cors imp fix

--- v1.0/backend/src/app.js
+++ v1.0/backend/src/app.js
@@ -22,6 +22,7 @@ const spec2 = fs.readFileSync(`${__dirname}/swagger-saral-maintenance.yaml`, 'ut
 const frontendSpec = yaml.load(spec);
 const maintenanceSpec = yaml.load(spec2);
 const app = express()
+app.disable("x-powered-by");
 
 app.use(expressWinston.logger({
    winstonInstance : logger,
@@ -53,28 +54,23 @@ const generateJestReportPdf = async () => {
     console.log("pdf printed")
 }
 
-const CORS_ORIGIN = [{ "origin": 'https://saral-dev-api.anuvaad.org', "optionsSuccessStatus": 200, "methods": ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] },{ "origin": 'https://saral-api.anuvaad.org', "optionsSuccessStatus": 200, "methods": ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }, { "origin": 'https://saral-api.anuvaad.org', "optionsSuccessStatus": 200, "methods": ['GET', 'POST', 'DELETE', 'UPDATE', 'PUT', 'PATCH'] }]
-
-const checkURL = (req, res, next) => {
-    const { origin, methods } = req.headers;
-    let isUrlExist = CORS_ORIGIN.findIndex((el) => { return el.origin == origin });
-    let isMethodAvailable = methods ? methods.toUpperCase() : "";
-
-    let isMethodExist = CORS_ORIGIN[isUrlExist] ? CORS_ORIGIN[isUrlExist].methods.findIndex((el) => { return el == isMethodAvailable }) : -1;
-    
-    if (isUrlExist == -1) {
-      return res.status(403).json({ error: 'Invalid URL' });
-    } else if (isMethodExist == -1) {
-        return res.status(403).json({ error: 'Invalid Method' });
+const whitelist = ['https://saral-dev-api.anuvaad.org', 'https://saral-api.anuvaad.org', 'https://saral-api.anuvaad.org']
+const corsOptions = {
+  origin: (origin, callback) => {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Invalid URL'))
     }
-    next();
-  };
+  },
+  methods: 'GET',
+  optionsSuccessStatus: 200
+}
 
 app.use(express.json({ limit: '50mb' }));
 app.use(express.urlencoded({ limit: "50mb", extended: true, parameterLimit: 50000 }))
 app.use(express.json())
-app.use(cors());
-app.use(checkURL)
+app.use(cors(corsOptions));
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 // Register the function as middleware for the application
@@ -90,6 +86,16 @@ app.use(brandRouter)
 app.use(db.releaseClientPool)
 app.use("/api-docs/saral/frontend", swaggerUi.serve, (...args) => swaggerUi.setup(frontendSpec)(...args));
 app.use("/api-docs/saral/maintenance", swaggerUi.serve, (...args) => swaggerUi.setup(maintenanceSpec)(...args));
+
+//error handling middleware
+app.use((err, req, res, next) => {
+    if(err.message == 'Invalid URL') {
+        return res.status(403).json({ error: 'Invalid URL' });
+    } else {
+        return res.status(500).json({ error: 'Internal Server Error'});
+    }
+    
+});
 
 // uncomment below function to generate jest report pdf 
 // generateJestReportPdf() 
