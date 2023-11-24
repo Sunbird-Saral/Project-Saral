e19044a47ceb430e5f3c3913c5067c2caaa8984d
sanjay-v46 <sanjay@tarento.com>
2023-11-24 10:47:54 +0530

    SS-639: added admissions API routes and collection schema

---
+++ v1.0/backend/src/controller/admissionsController.js
@@ -0,0 +1,13 @@
+const admissionsSchema = require('../models/admissions')
+
+exports.saveAdmissions = async (req, res, next) => {
+
+    //TODO add business logic
+    res.status(200).json("success")
+}
+
+exports.getAdmissions = async (req, res, next) => {
+
+    //TODO add business logic
+    res.status(200).json("success")
+}
---
+++ v1.0/backend/src/routers/admission.route.js
@@ -0,0 +1,10 @@
+const express = require('express')
+const { auth } = require('../middleware/auth')
+const router = new express.Router()
+const admissionsController = require('../controller/admissionsController')
+
+
+router.put('/admissions',auth,admissionsController.saveAdmissions)
+router.get('/admissions', auth,admissionsController.getAdmissions)
+
+module.exports = router
